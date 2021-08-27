525043930b5d297e9543e627e373da6f67db340f
Andrei Bodrov <a.bodrov@hybrid.ai>
2021-08-27 15:24:47 +0300

    firebase added

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore' 
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC_1wVxIPRjjnkKlJxXAMNxkdOPCFkfQp0",
+    authDomain: "chat-eb446.firebaseapp.com",
+    projectId: "chat-eb446",
+    storageBucket: "chat-eb446.appspot.com",
+    messagingSenderId: "595805092503",
+    appId: "1:595805092503:web:9f7c087ea603610de607d2",
+    measurementId: "G-BMQ96N0DCT"
+  };
+//init firebase
+  firebase.initializeApp(firebaseConfig)
+//init firestore service
+const projectFirestore = firebase.firestore()
+const projectFirestore = firebase.firestore.FieldValue.serverTimestamp
+export { projectFirestore }
