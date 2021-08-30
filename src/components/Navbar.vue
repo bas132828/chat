f34e2dfad60624722021cdb72a7db8cc88720887
Andrei Bodrov <a.bodrov@hybrid.ai>
2021-08-30 18:02:35 +0300

    navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav>
+      <div>
+          <p>Hey there ... name here</p>
+          <p class="email"> currently logged in as ... email</p>
+      </div>
+      <button>Logout</button>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
