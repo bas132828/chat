971775679dae12d05c054ebd43f8429a30843e91
Andrei Bodrov <a.bodrov@hybrid.ai>
2021-09-24 10:45:47 +0300

    modes added

--- src/components/NewChatForm.vue
+++ src/components/NewChatForm.vue
@@ -1,12 +1,16 @@
 <template>
   <form>
+    <input type="radio" id="regular" value="regular" v-model="mode" />
+    <input type="radio" id="hard" value="hard" v-model="mode" />
+    <input type="radio" id="soft" value="soft" v-model="mode" />
+    <span> {{ mode }} mode</span>
     <textarea
       @keypress.enter.prevent="handleSubmit"
       placeholder="Type your message and hit enter here"
       v-model="message"
     ></textarea>
+    <div class="error">{{ error }}</div>
   </form>
-  <div class="error">{{ error }}</div>
 </template>
 
 <script>
@@ -15,50 +19,62 @@ import getUser from "../composables/getUser";
 import useCollection from "../composables/useCollection";
 import { timestamp } from "../firebase/config";
 import arr from "../composables/getWord";
+import arr2 from "../composables/getGoodWord";
 
 export default {
   setup() {
     const message = ref("");
     const { user } = getUser();
     const { addDoc, error } = useCollection("messages");
+    const mode = ref("regular");
 
     const handleSubmit = async () => {
-      function getRandomArbitrary() {
-        const num = Math.trunc(Math.random() * arr.length);
-        return arr[num];
+      function getRandomWordFromArray() {
+        if (mode.value === "hard") {
+          const num = Math.trunc(Math.random() * arr.length);
+          return arr[num];
+        }
+        if (mode.value === "soft") {
+          const num = Math.trunc(Math.random() * arr2.length);
+          return arr2[num];
+        }
       }
-
       let finalMessage = "";
       if (message.value === "") return;
       finalMessage = message.value;
 
-      if (message.value.includes(",")) {
-        const temp = finalMessage.split(",").map((el, i, array) => {
-          if (i < array.length - 1) return `${el}, ${getRandomArbitrary()},`;
-          else return el;
-        });
-        finalMessage = temp.join(" ");
-      }
+      if (mode.value !== "regular") {
+        if (message.value.includes(",")) {
+          const temp = finalMessage.split(",").map((el, i, array) => {
+            if (i < array.length - 1)
+              return `${el}, ${getRandomWordFromArray()},`;
+            else return el;
+          });
+          finalMessage = temp.join(" ");
+        }
 
-      if (message.value.includes("!")) {
-        const temp = finalMessage.split("!").map((el, i, array) => {
-          if (i < array.length - 1) return `${el}, ${getRandomArbitrary()}!`;
-          else return el;
-        });
-        finalMessage = temp.join(" ");
-      }
-      if (message.value.includes("?")) {
-        finalMessage = `${finalMessage.replaceAll(
-          "?",
-          ""
-        )}, ${getRandomArbitrary()}?`;
-      }
-      if (message.value.includes(".")) {
-        const temp = finalMessage.split(".").map((el, i, array) => {
-          if (i < array.length - 1) return `${el}, ${getRandomArbitrary()}.`;
-          else return el;
-        });
-        finalMessage = temp.join(" ");
+        if (message.value.includes("!")) {
+          const temp = finalMessage.split("!").map((el, i, array) => {
+            if (i < array.length - 1)
+              return `${el}, ${getRandomWordFromArray()}!`;
+            else return el;
+          });
+          finalMessage = temp.join(" ");
+        }
+        if (message.value.includes("?")) {
+          finalMessage = `${finalMessage.replaceAll(
+            "?",
+            ""
+          )}, ${getRandomWordFromArray()}?`;
+        }
+        if (message.value.includes(".")) {
+          const temp = finalMessage.split(".").map((el, i, array) => {
+            if (i < array.length - 1)
+              return `${el}, ${getRandomWordFromArray()}.`;
+            else return el;
+          });
+          finalMessage = temp.join(" ");
+        }
       }
       ///
       const chat = {
@@ -73,7 +89,7 @@ export default {
       }
     };
 
-    return { message, handleSubmit, error };
+    return { message, handleSubmit, error, mode };
   },
 };
 </script>
---
+++ src/composables/getGoodWord.js
@@ -0,0 +1,4 @@
+const init = "добро, солнышко, цветочек";
+
+const arr2 = init.split(",");
+export default arr2;
